1591bcaf1dadd8b3b7a1830abe7778bdaebd755b
AMeyer33 <andrew.meyer@stu.daviess.kyschools.us>
2026-02-04 17:53:51 +0000

    Commit

--- TokenPass.java
+++ TokenPass.java
@@ -1,9 +1,22 @@
 public TokenPass(int playerCount){
   /*  to be answered in part (a) */
-  
+  ArrayList <Integer> board = new ArrayList <Integer>();
+  board.size().equals(playerCount);
+  int currentPlayer = (int)(Math.random()*playerCount-1);
+  for(int x: board)
+  {
+    Random rand = new Random();
+    board.add(rand.nextInt(10));
+  }
 }
 
 public void distributeCurrentPlayerTokens(){
   /* to be answered in part (b) */
+  for(int x = currentPlayer; x < 0; x++;)
+  {
+    currentPlayer(x) ++;
+    
+  }
+  
   
 }
